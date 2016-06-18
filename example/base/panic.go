32cb50adbf10a543f0f66eae963c7d6082db23d5
Joey Fang <josudoey@gmail.com>
2016-06-18 20:11:38 +0800

    :q

---
+++ example/base/panic.go
@@ -0,0 +1,30 @@
+// ref https://blog.golang.org/defer-panic-and-recover
+package main
+
+import "fmt"
+
+func g(i int) {
+	if i > 3 {
+		fmt.Println("Panicking!")
+		panic(fmt.Sprintf("%v", i))
+	}
+	defer fmt.Println("Defer in g", i)
+	fmt.Println("Printing in g", i)
+	g(i + 1)
+}
+
+func f() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	fmt.Println("Calling g.")
+	g(0)
+	fmt.Println("Returned normally from g.")
+}
+
+func main() {
+	f()
+	fmt.Println("Returned normally from f.")
+}
