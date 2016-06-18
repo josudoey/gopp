7046886fc8195bb8f8dbff10a4a38f83326ae996
Joey Fang <josudoey@gmail.com>
2016-06-18 16:53:58 +0800

    [modify] http request example.

--- example/http/get.go
+++
@@ -1,6 +0,0 @@
-//ref https://golang.org/pkg/net/http/
-package main
-import "net/http"
-func main() {
-    resp, err := http.Get("http://example.com/")
-}
---
+++ example/http/request-dump.go
@@ -0,0 +1,46 @@
+//ref https://golang.org/pkg/net/http/httputil/#example_DumpRequest
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/http/httputil"
+	"strings"
+)
+
+func main() {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		dump, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "%q", dump)
+	}))
+	defer ts.Close()
+	fmt.Println("Test Server Listen on ", ts.URL)
+
+	const body = "Go is a general-purpose language designed with systems programming in mind."
+	req, err := http.NewRequest("POST", ts.URL, strings.NewReader(body))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Host = "www.example.org"
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", b)
+
+}
