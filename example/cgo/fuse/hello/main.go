1175eec37d8d88ad0167a84ce39efb5b16308422
Joey Fang <josudoey@gmail.com>
2016-06-25 12:10:50 +0800

    [modify[ add fuse example

---
+++ example/cgo/fuse/hello/main.go
@@ -0,0 +1,22 @@
+package main
+
+// #cgo CFLAGS: -D_FILE_OFFSET_BITS=64 -I/usr/include/fuse  -pthread
+// #cgo LDFLAGS: -lfuse
+//
+// #include "hello.h"
+import "C"
+import "flag"
+import "log"
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 1 {
+		log.Fatal("Usage:\n  hello [MOUNTPOINT]")
+	}
+
+	mountPoint := flag.Arg(0)
+	if mountPoint == "" {
+		log.Fatal("Usage:\n  hello [MOUNTPOINT]")
+	}
+	C.helloMount(C.CString(mountPoint))
+}
