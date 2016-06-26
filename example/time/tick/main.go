b532b01bb5fa17708bb968193d8c45011970d4b4
Joey Fang <josudoey@gmail.com>
2016-06-26 09:25:18 +0800

    [modify] tick example.

--- example/time/tick/main.go
+++ example/time/tick/main.go
@@ -7,7 +7,11 @@ import (
 
 func main() {
 	c := time.Tick(1 * time.Second)
+	t := time.Now().Unix() + 3
 	for now := range c {
 		fmt.Printf("%v %v\n", now.UnixNano(), now)
+		if now.Unix() > t {
+			break
+		}
 	}
 }
