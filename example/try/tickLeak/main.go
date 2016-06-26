bcf31084e02d5187e3ff502fd346d4e2f455d227
Joey Fang <josudoey@gmail.com>
2016-06-26 10:15:50 +0800

    [add] tick example.

---
+++ example/try/tickLeak/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	var mem runtime.MemStats
+	fmt.Printf("%-10v %-15v %-15v %-15v %-15v %-15v\n", "time(ns)", "tick count", "Alloc", "TotalAlloc", "HeapAlloc", "HeapSys")
+	runtime.ReadMemStats(&mem)
+	i := 0
+	t0 := time.Now().UnixNano()
+	fmt.Printf("%-10v %-15v %-15v %-15v %-15v %-15v\n", time.Now().UnixNano()-t0, i, mem.Alloc, mem.TotalAlloc, mem.HeapAlloc, mem.HeapSys)
+	done := make(chan bool, 1)
+	go func() {
+		for ; i < 10000; i++ {
+			_ = time.Tick(time.Second)
+		}
+		done <- true
+	}()
+	_ = <-time.Tick(time.Millisecond)
+	runtime.ReadMemStats(&mem)
+	fmt.Printf("%-10v %-15v %-15v %-15v %-15v %-15v\n", time.Now().UnixNano()-t0, i, mem.Alloc, mem.TotalAlloc, mem.HeapAlloc, mem.HeapSys)
+	_ = <-done
+	runtime.ReadMemStats(&mem)
+	fmt.Printf("%-10v %-15v %-15v %-15v %-15v %-15v\n", time.Now().UnixNano()-t0, i, mem.Alloc, mem.TotalAlloc, mem.HeapAlloc, mem.HeapSys)
+}
